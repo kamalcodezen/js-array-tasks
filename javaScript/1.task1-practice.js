3d62e42205d0a5d3f5e6345802e30f9a02a4bc41
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 18:32:44 +0530

    array task 1 complete

---
+++ javaScript/1.task1-practice.js
@@ -0,0 +1,22 @@
+/**=================================
+ * 1. Declare an array.
+ 1.  Declare an array with 5 elements containing fruits.
+ 2.  console log the 3rd index element.
+ 3.  change the value of the 2nd index element to jambura.
+ 4.  console log the final array.
+ ===================================*/
+
+// 1. Declare an array. 
+//  1.  Declare an array with 5 elements containing fruits.
+let fruits = ["banana", "apple", "orange", "guava", "litchi"];
+
+// 2. console log the 3rd index element
+console.log(fruits[3]);
+
+// 3. change the value of the 2nd index element to jambura.
+let changeVal = fruits[2] = "jambura";
+console.log(changeVal);
+
+
+// 4.  console log the final array.
+console.log(fruits);
