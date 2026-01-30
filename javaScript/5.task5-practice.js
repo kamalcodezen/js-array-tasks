011e93cc4cb0559f633a9d6d6cd6e8ae755f6eab
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 19:21:08 +0530

    array.concat() method taks practice complete

---
+++ javaScript/5.task5-practice.js
@@ -0,0 +1,19 @@
+/****************************************
+  * 5. Combining Arrays
+  Instructions:
+
+ 1. Create two arrays of your choice.
+ 2. Use the concat method to combine the two arrays into a new array.
+ 3. Print both the original arrays and the combined array using console.log().
+ *****************************************/
+
+
+let city = ["delhi", "pune", "mumbai", "kolkata"];
+
+let marks = [78, 45, 86, 98, 40, 88];
+
+let addConcat = city.concat(marks);
+
+console.log(city);
+console.log(marks);
+console.log(addConcat);
