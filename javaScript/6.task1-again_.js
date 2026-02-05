54f9689b6f133101a64414c9834b9bff38273e46
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-06 04:19:43 +0530

    array task 1 comlpete

---
+++ javaScript/6.task1-again_.js
@@ -0,0 +1,12 @@
+/**
+ * Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+ */
+
+let fruits = ["apple", "litchi", "guava", "orange", "mango"];
+console.log(fruits[3]);
+let change = fruits[2] = "jambura";
+console.log(change);
+console.log(fruits);
