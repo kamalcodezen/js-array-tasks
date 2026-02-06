3061eb54011549e8055d93eb0363a3bcd77b56c3
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-06 13:03:41 +0530

    task 5 complete

---
+++ javaScript/10.task5-again.js
@@ -0,0 +1,20 @@
+/**
+ * Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+
+ */
+
+let cities = ["delhi", "pune", "mumbai", "kolkata"];
+let nums = [23, 45, 45, 8, 44, 75, 57];
+
+let joinArray = cities.concat(nums);
+
+console.log(cities);
+
+console.log(nums);
+
+console.log(joinArray);
